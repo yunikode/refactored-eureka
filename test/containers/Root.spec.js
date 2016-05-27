708c23ee9d4e081718e50624b5c704e8af958a8c
zee-german <franz.lutzenberger@gmail.com>
2016-05-27 14:32:51 +0200

    First Test with enzyme

---
+++ test/containers/Root.spec.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Root from '../../src/containers/Root'
+
+describe('(Container) Root', () => {
+  const wrapper = shallow(<Root />)
+
+  it('renders as a <div>', () => {
+    expect(wrapper.type()).to.be.eql('div')
+  })
+
+  it('has a style with height 100%', () => {
+    const expectedStyles = {
+      height: '100%',
+      background: '#333'
+    }
+    expect(wrapper.prop('style')).to.be.eql(expectedStyles)
+  })
+
+  it('contains a header explaining the app', () => {
+    expect(wrapper.find('.welcome-header')).to.have.length(1)
+  })
+})
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var webpack = require('webpack')
+var path = require('path')
+
+var config = {
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:3000',
+    './src/main'
+  ],
+  resolve: {
+    root: [
+      path.resolve(__dirname, './src')
+    ],
+    extensions: ['', '.js', '.json', '.jsx']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  }
+}
+
+module.exports = config
